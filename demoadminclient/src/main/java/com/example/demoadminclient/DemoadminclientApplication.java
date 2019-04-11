19bd9c80985ba6ff3464143ee51a1f90d292873f
wangliqiang <m18612165549@163.com>
2019-04-11 11:09:46 +0800

    first commit

---
+++ demoadminclient/src/main/java/com/example/demoadminclient/DemoadminclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoadminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoadminclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoadminclientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demoamind/DemoamindApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demoamind;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoamindApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
